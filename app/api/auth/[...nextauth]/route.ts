91219ed75b78d5fd01309d6922c9a87743affb2a
Nourasha <69981085+Nourasha@users.noreply.github.com>
2024-11-09 11:27:19 +0100

    Register

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,55 @@
-import NextAuth from "next-auth";
+import NextAuth, { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import GitHubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+import bycrypt from "bcrypt";
+import { use } from "react";
 
-export const authOptions = {
+const prisma = new PrismaClient();
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email", placeholder: "Email" },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "Password",
+        },
+      },
+      async authorize(credential, req) {
+        if (!credential?.email || !credential.password) return null;
+
+        const user = await prisma.user.findUnique({
+          where: { email: credential.email },
+        });
+        if (!user) return null;
+
+        const passWordsMatch = await bycrypt.compare(
+          credential.password,
+          user.hashedPassWord!
+        );
+
+        return passWordsMatch ? use : null;
+      },
+    }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
   ],
+  session: {
+    strategy: "jwt",
+  },
 };
 const handler = NextAuth(authOptions);
 
---
+++ prisma/migrations/20241109090159_hashed/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `users` ADD COLUMN `hashedPassWord` VARCHAR(191) NULL;
